78f4fa70d66347914b698435ab53868edba3dd03
GreybeardGM <webmaster@sector-001.de>
2025-08-08 13:34:03 +0200

    Update module.js

--- scripts/module.js
+++ scripts/module.js
@@ -1,6 +1,20 @@
 import { applyFrameToToken } from "./apply-frame.js";
 
 Hooks.once("init", () => {
+  game.settings.register("greybearded-tokens", "frameScale", {
+    name: "Token Frame Scale",
+    hint: "Verändert die Größe des Tokenrahmens relativ zum Token selbst. 1 = exakt gleich groß, 1.1 = etwas größer.",
+    scope: "client",
+    config: true,
+    type: Number,
+    default: 1,
+    range: {
+      min: 0.5,
+      max: 2,
+      step: 0.05
+    }
+  });
+  
   console.log("✅⭕ Greybearded Token Frames initialized.");
 });
 
