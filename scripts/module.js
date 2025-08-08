e00e84091708780a6c8c9e69aace8aa3370933f9
GreybeardGM <webmaster@sector-001.de>
2025-08-08 11:30:37 +0200

    Update module.js

--- scripts/module.js
+++ scripts/module.js
@@ -1,33 +1,37 @@
 import { getTintColor } from "./get-tint-color.js";
 
-Hooks.on("renderToken", (token, html) => {
-  const framePath = "modules/greybearded-tokens/assets/frame-default.png";
-  const tint = getTintColor(token);
+Hooks.once("init", () => {
+  console.log("Greybearded Token Frames initialized.");
 
-  const img = document.createElement("img");
-  img.src = framePath;
-  img.classList.add("token-frame-overlay");
+  Hooks.on("renderToken", (token, html) => {
+    console.log("renderToken hook fired for", token.name);
 
-  // Mitskalierend positionieren
-  Object.assign(img.style, {
-    position: "absolute",
-    top: "0",
-    left: "0",
-    width: "100%",
-    height: "100%",
-    objectFit: "contain",
-    pointerEvents: "none",
-    zIndex: "20",
+    const framePath = "modules/greybearded-tokens/assets/frame-default.png";
+    const tint = getTintColor(token);
 
-    // Tint mit Maskierung
-    backgroundColor: tint,
-    maskImage: `url(${framePath})`,
-    webkitMaskImage: `url(${framePath})`,
-    maskSize: "100% 100%",
-    webkitMaskSize: "100% 100%",
-    mixBlendMode: "multiply",
-    opacity: "0.9",
+    const img = document.createElement("img");
+    img.src = framePath;
+    img.classList.add("token-frame-overlay");
+
+    Object.assign(img.style, {
+      position: "absolute",
+      top: "0",
+      left: "0",
+      width: "100%",
+      height: "100%",
+      objectFit: "contain",
+      pointerEvents: "none",
+      zIndex: "20",
+
+      backgroundColor: tint,
+      maskImage: `url(${framePath})`,
+      webkitMaskImage: `url(${framePath})`,
+      maskSize: "100% 100%",
+      webkitMaskSize: "100% 100%",
+      mixBlendMode: "multiply",
+      opacity: "0.9"
+    });
+
+    html.append(img);
   });
-
-  html.append(img);
 });
