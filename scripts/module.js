061d8a1dc771812ccfeccd8182ce82064ebd7aa7
GreybeardGM <webmaster@sector-001.de>
2025-08-08 11:52:27 +0200

    Update module.js

--- scripts/module.js
+++ scripts/module.js
@@ -1,10 +1,15 @@
 import { getTintColor } from "./get-tint-color.js";
 
 Hooks.once("init", () => {
-  console.log("Greybearded Token Frames initialized.");
+  console.log("✅⭕Greybearded Token Frames initialized.");
+});
+
+// ACHTUNG: renderToken-Hook muss *nach* canvas existieren!
+Hooks.once("ready", () => {
+  console.log("✅⭕Greybearded Token Frames ready.");
 
   Hooks.on("renderToken", (token, html) => {
-    console.log("renderToken hook fired for", token.name);
+    console.log("⭕renderToken hook fired for", token.name);
 
     const framePath = "https://assets.forge-vtt.com/6409126bc31700d40e3ac139/Dungeon%20World/Tokens/Frames/player.png";
     const tint = getTintColor(token);
