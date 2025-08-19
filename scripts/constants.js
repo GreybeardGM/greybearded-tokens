5a4322d66f9e829eff3769759212fb8936f1b6e5
GreybeardGM <webmaster@sector-001.de>
2025-08-19 17:34:34 +0200

    Update constants.js

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,29 +1,5 @@
 export const MOD_ID = "greybearded-tokens";
 
-export const SETTING_KEYS = {
-  defaultFrameColor: "defaultFrameColor",
-
-  // Frame 1
-  frameImagePath: "frameImagePath",
-  frameScale: "frameScale",
-  frameTintMode: "frameTintMode",
-  usePlayerColor1: "usePlayerColor1",
-
-  // Frame 2
-  secondaryFrameEnabled: "secondaryFrameEnabled",
-  secondaryFrameImagePath: "secondaryFrameImagePath",
-  secondaryFrameTintMode: "secondaryFrameTintMode",
-  secondaryFrameScale: "secondaryFrameScale",
-  usePlayerColor2: "usePlayerColor2",
-
-  // Disposition-Farben
-  colorHostile: "color-hostile",
-  colorNeutral: "color-neutral",
-  colorFriendly: "color-friendly",
-  colorSecret: "color-secret",
-  colorCharacter: "color-character",
-};
-
 // Tint Choices
 export const TINT_CHOICES = {
   Disposition: "Disposition",
