d0e7aeeff40af2c7b8fa392e8d327e1c16995a6c
GreybeardGM <webmaster@sector-001.de>
2025-08-19 18:29:43 +0200

    Update module.js

--- scripts/module.js
+++ scripts/module.js
@@ -1,8 +1,10 @@
 import { registerSettings } from "./settings.js";
 import { registerRenderingHooks } from "./hooks.js";
+import { buildSnapshot } from "./settings-snapshot.js";
 
 Hooks.once("init", () => {
   registerSettings();
+  buildSnapshot();
 });
 
 /*
