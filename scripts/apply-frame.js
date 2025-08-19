14f26815f083451e243284183f1f054b6ea490df
GreybeardGM <webmaster@sector-001.de>
2025-08-19 11:22:00 +0200

    Update apply-frame.js

--- scripts/apply-frame.js
+++ scripts/apply-frame.js
@@ -1,6 +1,7 @@
 // apply-frame.js
 import { getTintColor } from "./get-tint-color.js";
 import { getGbFrameSettings } from "./settings-snapshot.js";
+import { dbg } from "./debug.js";
 
 export async function applyFrameToToken(token) {
   if (token.document.getFlag("greybearded-tokens", "disableFrame")) return;
@@ -42,10 +43,12 @@ export async function applyFrameToToken(token) {
   // Tints (Mode fix aus Snapshot; Wert dynamisch je Token-Zustand)
   {
     const t1 = getTintColor(token, S.mode1);
+    dbg("apply primary tint", { token: token.name, id: token.id, mode, color: t1 });
     frame1.tint = t1 ? PIXI.utils.string2hex(t1) : 0xFFFFFF;
 
     if (frame2) {
       const t2 = getTintColor(token, S.mode2);
+      dbg("apply secondary tint", { token: token.name, id: token.id, mode, color: t2 });
       frame2.tint = t2 ? PIXI.utils.string2hex(t2) : 0xFFFFFF;
     }
   }
