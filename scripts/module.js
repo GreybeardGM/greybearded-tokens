59a91ff874c29f840692c056f146c5b2fbb2db82
GreybeardGM <webmaster@sector-001.de>
2025-08-25 18:10:06 +0200

    Update module.js

--- scripts/module.js
+++ scripts/module.js
@@ -2,14 +2,24 @@ import { registerSettings } from "./settings.js";
 import { registerRenderingHooks } from "./hooks.js";
 import { buildSnapshot } from "./settings-snapshot.js";
 
+import { applyFrameToToken } from "./apply-frame.js";
+import { applyMaskToToken, clearMask } from "./apply-mask.js";
+import { buildSnapshot, getGbFrameSettings } from "./settings-snapshot.js";
+
+Hooks.once("ready", () => {
+  const mod = game.modules.get("greybearded-tokens");
+  mod.api = {
+    applyFrameToToken,
+    applyMaskToToken,
+    clearMask,
+    buildSnapshot,
+    getGbFrameSettings
+  };
+  console.log("GBT api ready:", Object.keys(mod.api));
+});
+
 Hooks.once("init", () => {
   registerSettings();
   buildSnapshot();
   registerRenderingHooks();
 });
-
-/*
-Hooks.once("setup", () => {
-  // Platzhalter, falls später notwendig (z.B. i18n vorbereiten)
-});
-*/
