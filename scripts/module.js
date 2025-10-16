f8d938516331d40b775ae7df2e5ada76535f0a8f
GreybeardGM <webmaster@sector-001.de>
2025-10-16 20:41:09 +0200

    Update module.js

--- scripts/module.js
+++ scripts/module.js
@@ -6,20 +6,23 @@ import { applyFrameToToken } from "./apply-frame.js";
 import { applyMaskToToken, clearMask } from "./apply-mask.js";
 import { getGbFrameSettings } from "./settings-snapshot.js";
 
+Hooks.once("init", () => {
+  registerSettings();
+  registerRenderingHooks();
+});
+
 Hooks.once("ready", () => {
+  buildSnapshot(); // jetzt sind persistierte Settings sicher geladen
   const mod = game.modules.get("greybearded-tokens");
-  mod.api = {
-    applyFrameToToken,
-    applyMaskToToken,
-    clearMask,
-    buildSnapshot,
-    getGbFrameSettings
-  };
+  mod.api = { applyFrameToToken, applyMaskToToken, clearMask, buildSnapshot, getGbFrameSettings };
   console.log("GBT api ready:", Object.keys(mod.api));
 });
 
-Hooks.once("init", () => {
-  registerSettings();
-  buildSnapshot();
-  registerRenderingHooks();
+Hooks.on("canvasReady", () => {
+  buildSnapshot(); // falls Welt/Scene-spezifische Settings/Fallbacks
+});
+
+// Optional: bei Settings-Änderung Snapshot erneuern
+Hooks.on("updateSetting", (setting) => {
+  if (setting?.key?.startsWith("greybearded-tokens.")) buildSnapshot();
 });
