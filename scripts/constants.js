7224289ca549d088ae2a6fb0a382163e26a6ea75
GreybeardGM <webmaster@sector-001.de>
2025-10-29 07:29:22 +0100

    Update constants.js

--- scripts/constants.js
+++ scripts/constants.js
@@ -8,3 +8,16 @@ export const TINT_CHOICES = {
   Advanced: "Advanced",
   NoTint: "NoTint"
 };
+
+// Font Choices
+export const FONT_CHOICES = {
+  "Arial": "Arial",
+  "Amiri": "Amiri",
+  "Bruno Ace": "Bruno Ace",
+  "Courier": "Courier",
+  "Courier New": "Courier New",
+  "Modesto Condensed": "Modesto Condensed",
+  "Signika": "Signika",
+  "Times": "Times",
+  "Times New Roman": "Times New Roman"
+};
