28fd32e1bd88d7b56bb62a0797dd0d73ec60b5e6
GreybeardGM <webmaster@sector-001.de>
2026-02-16 18:26:15 +0100

    Centralize initial snapshot build in canvasReady hook

--- scripts/module.js
+++ scripts/module.js
@@ -1,18 +1,8 @@
 // module.js
 import { registerSettings } from "./settings/register.js";
 import { registerRenderingHooks } from "./hooks.js";
-import { buildSnapshot } from "./settings/snapshot.js";
 
 Hooks.once("init", () => {
   registerSettings();
   registerRenderingHooks(); // Register rendering hooks before other startup work
-});
-
-Hooks.once("ready", () => {
-  buildSnapshot(); // Build initial settings snapshot once Foundry is ready
-});
-
-Hooks.on("canvasReady", () => {
-  // Refresh cached settings when scenes/world state change
-  buildSnapshot();
 });
